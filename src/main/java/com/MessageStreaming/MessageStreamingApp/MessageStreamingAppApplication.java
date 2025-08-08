a32fb0be83c66ce5715102c7d73fcb0d09864536
victorhabila <48987601+victorhabila@users.noreply.github.com>
2025-08-08 13:35:56 +0200

    Project setup

---
+++ src/main/java/com/MessageStreaming/MessageStreamingApp/MessageStreamingAppApplication.java
@@ -0,0 +1,13 @@
+package com.MessageStreaming.MessageStreamingApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageStreamingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessageStreamingAppApplication.class, args);
+	}
+
+}
